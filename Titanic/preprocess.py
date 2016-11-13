c0e101ade439548142085beec5da0fe3d2e9afdd
Maroof <maroofmf@gmail.com>
2016-11-12 23:19:49 -0800

    Updated Readme and creted preprocess

---
+++ Titanic/preprocess.py
@@ -0,0 +1,16 @@
+'''
+Author: Maroof
+Dataset: https://www.kaggle.com/c/titanic/data
+'''
+
+''' Import all libraries '''
+import numpy as np
+import pandas as pd
+import matplotlib.pylab as plt
+
+
+# class preprocess:
+
+    # def __init__(self):
+
+
